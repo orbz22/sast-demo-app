f048fc1f7c7f8660b7f1a8b12e2aad33307376c8
orbz22 <raihanmajid225@gmail.com>
2025-05-05 06:19:48 +0000

    Update app.py
    apaaja

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import subprocess
+import shlex
 
 def greet(name):
     print(f"Hello, {name}!")
 
 def run_command(cmd):
-    subprocess.call(cmd, shell=True)  # Kerentanannya ada di sini, penggunaan shell=True tanpa validasi input.
+    safe_cmd = shlex.split(cmd)
+    subprocess.call(safe_cmd)
 
 if __name__ == "__main__":
     name = input("Enter your name: ")
     greet(name)
-
     cmd = input("Enter a command to run: ")
     run_command(cmd)
-
